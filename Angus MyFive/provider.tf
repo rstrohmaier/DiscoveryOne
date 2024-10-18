d482ce8b12f0c9c0dbbfdef4e95b91113401deb1
Mario Strobl <166598774+ITsMaSt@users.noreply.github.com>
2024-10-18 09:04:28 +0000

    Added provider

--- Angus MyFive/provider.tf
+++ Angus MyFive/provider.tf
@@ -14,3 +14,8 @@ provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "bumblebee-resource"
+  location = "West Europe"
+}
